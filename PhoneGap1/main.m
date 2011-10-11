d7441b8bf6906c1a4b627325a4afa9590252e3b7
DenD <davyleemans@Davy-Leemanss-iMac.local>
2011-10-11 21:36:21 +0200

    Initial Commit

---
+++ PhoneGap1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PhoneGap1
+//
+//  Created by DenD on 11/10/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
